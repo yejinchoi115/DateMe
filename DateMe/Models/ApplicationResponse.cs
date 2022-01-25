caaa61b9d8649b3af15340fb5721137995c52ceb
yejinchoi115 <yjjjang115@gmail.com>
2022-01-24 19:33:08 -0700

    Initial check-in of module DateMe

---
+++ DateMe/Models/ApplicationResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DateMe.Models
+{
+    public class ApplicationResponse
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public byte Age  { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Major { get; set; }
+
+        public bool CreeperStalker { get; set; }
+
+    }
+}
